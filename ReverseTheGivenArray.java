50cc76d40d912e7aa2eeed662d4bd0e927a0830d
BhavyaUM <123004773+BhavyaUM@users.noreply.github.com>
2023-06-09 10:11:26 +0530

    Add files via upload

---
+++ ReverseTheGivenArray.java
@@ -0,0 +1,10 @@
+package mostimportant;
+
+public class ReverseTheGivenArray {
+public static void main(String[] args) {
+	int a[]= {10,30,50,24};
+	for(int i=a.length-1;i>=0;i--) {
+		System.out.println(a[i]);
+	}
+}
+}
